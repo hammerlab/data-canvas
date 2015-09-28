47b8a06b87815d348f1570c80a7d7821e89014da
Dan Vanderkam <danvdk@gmail.com>
2015-09-28 16:32:08 -0400

    Initial port from pileup.js

---
+++ types.js
@@ -0,0 +1,29 @@
+// These are type declarations for use with Flow
+// http://flowtype.org/
+
+declare class DataCanvasRenderingContext2D extends CanvasRenderingContext2D {
+  pushObject(o: any): void;
+  popObject(): void;
+  reset(): void;
+}
+
+declare class DataContext extends DataCanvasRenderingContext2D {
+  constructor(ctx: CanvasRenderingContext2D): void;
+}
+
+declare class RecordingContext extends DataCanvasRenderingContext2D {
+  constructor(ctx: CanvasRenderingContext2D): void;
+  calls: Object[];
+  drawnObjectsWith(predicate: (o: Object)=>boolean): Object[];
+  callsOf(type: string): Array<any>[];
+
+  static recordAll(): void;
+  static reset(): void;
+  static drawnObjectsWith(div: HTMLElement, selector: string, predicate:(o: Object)=>boolean): Object[];
+  static callsOf(div: HTMLElement, selector: string, type: string): Array<any>[];
+}
+
+declare class ClickTrackingContext extends DataCanvasRenderingContext2D {
+  constructor(ctx: CanvasRenderingContext2D, x: number, y: number): void;
+  hits: any[][];
+}
